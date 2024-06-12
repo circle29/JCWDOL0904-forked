7204fd019bfa5170c53347882617c3f770df4319
circle29 <alexiusnicholas@rocketmail.com>
2024-06-12 15:12:09 +0700

    update config.js

---
+++ projects/server/src/config/config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+const mysql2 = require("mysql2");
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_SCHEMA,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    dialectModule: mysql2,
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_SCHEMA,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    dialectModule: mysql2,
+  },
+};
