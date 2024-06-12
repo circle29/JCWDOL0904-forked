97d1faa34fe7d41910be60d19f5b1f83769e968b
circle29 <alexiusnicholas@rocketmail.com>
2024-06-12 15:43:14 +0700

    change back index.js and verceljson

--- projects/server/src/index.js
+++ projects/server/src/index.js
@@ -33,15 +33,7 @@ module.exports = { io };
 
 // console.log(process.env.WHITELISTED_DOMAIN);
 
-const corsConfig = {
-  origin: "",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-};
-app.use(cors(corsConfig));
-app.options("", cors(corsConfig));
-
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(express.static(join(__dirname, "public/images")));
 app.use(bodyParser.json());
